ab2da9b121bc52a41c49a24e5b2314c493434629
BuildTools <unconfigured@null.spigotmc.org>
2019-08-04 22:11:26 -0500

    Add README
    Add a readme and tell people how to contribute

---
+++ README.md
@@ -0,0 +1,10 @@
+# 21 plus 58
+### A CAS library for use with VexV5 and PROS
+
+***This project is not stale. The CAS will be developed as needed or when Drew gets bored enough to update it.***
+
+## Usage
+This project currently has no API as it is empty. Try requesting a feature.
+
+## Requesting Features
+To request a feature, create an issue with the request. Alternatively, you can add in the feature yourself, test it, and then submit a pull request. If you choose to do that, thanks.
---
+++ include/21+58.h
@@ -0,0 +1,31 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// This file is part of the 21+58 CAS Library
+// This software is licensed under the MIT License
+// In the event that a copy of the MIT License was not included,
+// you can find it at https://opensource.org/licenses/MIT
+//
+// Filename: 21+58.h
+// Purpose:  Include file for using 21+58 with your project
+// Author:   Drew Deaton
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#include <tuple>
+
+namespace v5cas {
+  class EqSide {
+
+
+    double Evaluate(void);
+    double SetVariable(char varname, double value);
+  };
+}
+
+typedef std::tuple<v5cas::EqSide,v5cas::EqSide> TwoSideEquasion;
+
+
+
+v5cas::EqSide operator""_eqside (std::string istring) {
+
+}
